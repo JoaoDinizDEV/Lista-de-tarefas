d2a3e2f18b8bddcc98720ed81ede590d3556b1a2
João Diniz <joaovitordinizpereira081@gmail.com>
2025-05-07 23:21:41 -0300

    Finalizado

---
+++ js/controle.js
@@ -0,0 +1,58 @@
+
+let contador = 0;
+let input = document.getElementById('inputTarefa');
+let btnAdd = document.getElementById('btn-add');
+let main = document.getElementById('areaLista');
+
+function addTarefa() {
+    let valorInput = input.value;
+
+    if((valorInput !=="") && (valorInput !== null) && (valorInput !== undefined)){
+       
+        ++contador; 
+
+        let novoItem = `<div id="${contador}" class="item">  
+            <div onclick="marcarTarefa(${contador})" class="item-nome">
+                ${valorInput}
+            </div>
+
+            <div class="item-botao">
+                <button onclick="deletar(${contador})" class="delete">Deletar</button>
+
+            </div>
+        </div>`;
+
+        main.innerHTML += novoItem; 
+
+        input.value ="";
+        input.focus();
+
+    }
+}
+
+
+function deletar(id){
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+function marcarTarefa(id){
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+
+    if(classe =="item"){
+        item.classList.add('clicado');
+
+        item.parentNode.appendChild(item);
+
+    }else{
+        item.classList.remove('clicado');
+    }
+}
+
+input.addEventListener("keyup", function(event){
+    if(event.keyCode === 13){
+        event.preventDefault();
+        btnAdd.click();
+    }
+})
